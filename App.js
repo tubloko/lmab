d712a605ba2fc7210d73f1f0f5d206b570b8c17d
ruslan.shukiurov <sukurovruslan68@gmail.com>
2021-06-18 22:14:02 +0300

    add facebook sdk, add react-native-fbsdk-next fcb login button works

--- App.js
+++ App.js
@@ -1,14 +1,50 @@
-import React from 'react';
+// import 'com.facebook.FacebookSdk';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   Text,
+  View
 } from 'react-native';
+import { LoginButton, AccessToken, Profile } from 'react-native-fbsdk-next';
 
 const App: () => React$Node = () => {
+  const [login, setLogin] = useState(null);
+  console.log('login', login);
+  const currentProfile = Profile.getCurrentProfile().then(
+    function(currentProfile) {
+      if (currentProfile) {
+        console.log('The current logged user is: ' +
+          currentProfile.name
+          + '. His profile id is: ' +
+          currentProfile.userID
+        );
+      }
+    }
+  );
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ paddingVertical: 10, marginHorizontal: 10, }}>
       <Text>Welcome to lmab!</Text>
+      <View>
+        <LoginButton
+          onLoginFinished={
+            async (error, result) => {
+              if (error) {
+                console.log('login has error: ' + result.error);
+              } else if (result.isCancelled) {
+                console.log('login is cancelled.');
+              } else {
+                console.log('result', result);
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    setLogin(data.accessToken.toString());
+                  }
+                );
+              }
+            }
+          }
+          onLogoutFinished={() => console.log('logout.')}/>
+      </View>
     </SafeAreaView>
   );
 };
--- android/app/src/main/java/com/lmab/MainApplication.java
+++ android/app/src/main/java/com/lmab/MainApplication.java
@@ -1,5 +1,5 @@
 package com.lmab;
-
+import com.facebook.FacebookSdk;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
