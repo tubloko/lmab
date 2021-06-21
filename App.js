fd19e73cb20bed42b9c0c585f1f3b63f45f53ca3
ruslan.shukiurov <sukurovruslan68@gmail.com>
2021-06-21 18:29:12 +0300

    setup google sign in

--- App.js
+++ App.js
@@ -1,50 +1,15 @@
-// import 'com.facebook.FacebookSdk';
-import React, { useState } from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   Text,
-  View
 } from 'react-native';
-import { LoginButton, AccessToken, Profile } from 'react-native-fbsdk-next';
+import { AuthScreen } from './screens/AuthScreen';
 
 const App: () => React$Node = () => {
-  const [login, setLogin] = useState(null);
-  console.log('login', login);
-  const currentProfile = Profile.getCurrentProfile().then(
-    function(currentProfile) {
-      if (currentProfile) {
-        console.log('The current logged user is: ' +
-          currentProfile.name
-          + '. His profile id is: ' +
-          currentProfile.userID
-        );
-      }
-    }
-  );
-
   return (
-    <SafeAreaView style={{ paddingVertical: 10, marginHorizontal: 10, }}>
+    <SafeAreaView style={{ flex: 1, paddingVertical: 10, marginHorizontal: 10, }}>
       <Text>Welcome to lmab!</Text>
-      <View>
-        <LoginButton
-          onLoginFinished={
-            async (error, result) => {
-              if (error) {
-                console.log('login has error: ' + result.error);
-              } else if (result.isCancelled) {
-                console.log('login is cancelled.');
-              } else {
-                console.log('result', result);
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    setLogin(data.accessToken.toString());
-                  }
-                );
-              }
-            }
-          }
-          onLogoutFinished={() => console.log('logout.')}/>
-      </View>
+      <AuthScreen />
     </SafeAreaView>
   );
 };
